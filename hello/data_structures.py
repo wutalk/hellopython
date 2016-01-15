3c6ccc47bfa3175139cb697d09cf5ba0a1dad193
0wen <wutalk@sina.cn>
2016-01-15 17:34:13 +0800

    data strunctures demo

--- hello/data_structures.py
+++ hello/data_structures.py
@@ -1,6 +1,8 @@
-# List
+from math import pi
+
+# List [ ... ]
 # Kiwi QiYiGuo/MiHouTao, dragonfruit HuoLongGuo, loquats PiPa, lychee LiZhi,Pomelo YouZi
-fruits = ['kiwi', 'dragonfruit', 'loquats', 'lychee', 'Pomelo']
+fruits = ['kiwi', 'dragon fruit', 'loquats', 'lychee', 'Pomelo']
 print(fruits)
 fruits.append('apple')
 print(fruits)
@@ -10,4 +12,30 @@ biggerFruits.extend(fruits)
 print(biggerFruits)
 
 fruits.remove('apple')
+fruits.append('kiwi')
 print(fruits)
+# Set set([ ... ])
+
+print('lychee' in set(fruits))
+print(fruits.count('kiwi'))
+
+for i in range(1, 10):
+    print(str(round(pi, i))),
+print()
+
+# tuple ( .. )
+tuple = 128, 256, 512
+print(tuple)
+
+# dictionary { ... }
+tel = {'jack': 4098, 'sape': 4139}
+tel['guido'] = 4127
+print(tel)
+
+print(tel['sape'])
+print('jack in key', 'jack' in tel.keys())
+
+for i, v in tel.iteritems():
+    print i, v
+
+print('hello {}!'.format('owen'))
