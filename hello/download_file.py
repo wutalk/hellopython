bbdcc5aa5f86a5f56ecc1dbc0c2a87f437b17ee9
0wen <wutalk@sina.cn>
2016-12-15 10:08:10 +0800

    add more index

--- hello/download_file.py
+++ hello/download_file.py
@@ -1,4 +1,4 @@
-import urllib
+﻿# import urllib
 import time
 from datetime import date
 
@@ -10,14 +10,20 @@ proxies = {'ftp': 'http://10.144.1.10:8080/'}
 
 def download(fileCode):
     url = 'ftp://115.29.204.48/webdata/{}.xls'.format(fileCode)
-    f = 'd:/tmp/auto-download/{}_{}.xls'.format(fileCode, date.today())
+    f = 'D:/wutalk/portfolio/perf_data/{}_{}.xls'.format(fileCode, date.today())
     testfile = urllib.FancyURLopener(proxies)
     print 'start download ' + url + '...'
     testfile.retrieve(url, f)
-    print f + ' saved'
+    print 'saved to ' + f
 
+# 上证50 '000016.XSHG', 红利指数 '000015.XSHG', 中证红利 '000922.XSHG', 
+# 基本面50 '000925.XSHG', '000300.XSHG', '000905.XSHG'
+download('000015perf')
+download('000016perf')
+download('Csi922Perf')
+download('Csi925Perf')
 download('Csi300Perf')
 download('Csi905Perf')
 
-print 'download complete, exit within 5 seconds'
-time.sleep(5)
+print 'download complete, exit within 2 seconds'
+time.sleep(2)
